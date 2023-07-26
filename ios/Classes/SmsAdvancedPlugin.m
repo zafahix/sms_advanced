9cd77283f9bbd967a43845ff54c40958ff738679
henry <henry>
2023-07-27 01:39:54 +0700

    fix

--- ios/Classes/SmsAdvancedPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "SmsAdvancedPlugin.h"
-#import <sms_advanced/sms_advanced-Swift.h>
-
-@implementation SmsAdvancedPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SmsAdvancedPlugin registerWithRegistrar:registrar];
-}
-@end
